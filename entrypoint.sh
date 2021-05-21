6d6fe2dd46002d029f11f38ce1cf9bb17b4b3567
Pato Moreno <pamoreno@fi.uba.ar>
2021-05-21 20:05:47 -0300

    Primera versión del primer grader

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,28 @@
 #!/bin/sh -l
 
-cd $GITHUB_WORKSPACE/labs/datalab
-make $1 2>&1 | tee $1_result.log
-echo "::set-output name=result::$(< $1_result.log)"
+cd "$GITHUB_WORKSPACE/tp1" || exit
+command -v git || { apt update -y && apt install git -y ; }
+
+test_labfiles() {
+    # Remember current branch name
+    work_branch="$(git rev-parse --abbrev-ref HEAD)"
+    # Checkout PR target
+    git fetch origin "$GITHUB_BASE_REF" --depth=1 || return 0
+    git checkout "$GITHUB_BASE_REF" || return 0
+    mkdir -p /tmp || true
+    cp .labfiles /tmp/labfiles || return 0
+    git checkout "$work_branch"
+    git diff --name-only "origin/$GITHUB_BASE_REF..." >> /tmp/modified_files
+    if ! cmp /tmp/labfiles /tmp/modified_files ; then
+        body=$(grep -v -F -f /tmp/labfiles /tmp/modified_files)
+        echo "::set-output name=corrupt::true"
+        echo "::set-output name=modfiles::$body"
+        return 1
+    fi
+}
+
+test_labfiles || exit 1
+
+make "$1" | tee "$1".txt
+body=$(grep "Score = " "$1.txt")
+echo "::set-output name=result::$body"
